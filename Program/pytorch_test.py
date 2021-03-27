1882feca45b7a2a4056f160a97d6a97b09abe4f4
GimHuang <kim.huang.j@qq.com>
2021-03-27 09:56:13 +0800

    update program

---
+++ Program/pytorch_test.py
@@ -0,0 +1,11 @@
+'''
+Description: 
+version: V1.0
+Author: GimHuang
+Date: 2021-03-25 09:11:56
+LastEditors: GimHuang
+LastEditTime: 2021-03-25 11:03:33
+'''
+
+from __future__ import print_function
+import torch
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Deep_Learning_withPython
- Python 深度学习 学习笔记
+ :book:《Python 深度学习》 学习笔记
+
+:man:Author: Jin Huang	 :email:kim.huang.j@qq.com
+
+本文主要记录Python 深度学习 相关学习笔记 以及 修改后的代码等内容
+
